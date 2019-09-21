afb8a45163fc583e67dfba683997a2c9aa61c71a
Marcos Magalhães <marcosmagalhaes.ti@outlook.com>
2019-09-21 13:07:24 -0300

    DatabaseConfig

---
+++ src/main/java/br/com/sgr/conf/YAMLConfig.java
@@ -0,0 +1,12 @@
+package br.com.sgr.conf;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties
+public class YAMLConfig {
+
+}
